ecc35ede0e1d2526a4259de1a4f5274ef8b32f41
citry84-afk <citry84@gmail.com>
2026-08-08 14:18:56 +0200

    Actualiza cache PWA para diseño premium

--- cromomatch/sw.js
+++ cromomatch/sw.js
@@ -1,5 +1,5 @@
-const CACHE="cromomatch-dev-v2";
-const LOCAL=["./","index.html","style.css","app.js","manifest.webmanifest","icon.svg"];
+const CACHE="cromomatch-premium-v3";
+const LOCAL=["./","index.html","style.css","ultra.css","app.js","manifest.webmanifest","icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
